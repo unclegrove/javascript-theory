b89146c788d4f6bee097403bf51d19e1b689cd7a
Anton Alutin <mrunclegrove@gmail.com>
2022-08-01 19:26:00 +0300

    js-tasks

---
+++ js/module-one.js
@@ -0,0 +1,45 @@
+// let hugeNumber = 10000000;
+// console.log(hugeNumber);
+// hugeNumber = 1e7;
+// console.log(hugeNumber);
+
+// let smallNumber = 0.000001;
+// console.log(smallNumber);
+// smallNumber = 1e-6;
+// console.log(smallNumber);
+
+// console.log(Math.round(Math.random() * (10 - 0) + 0));
+
+/*let x = 6.35;
+console.log(Number(x.toFixed(1))); // Wrong rounding;
+console.log(Math.round(x * 10) / 10); // Correct rounding;
+*/
+
+// let i = 0;
+// while (i < 11) {
+//   i += 0.2;
+//   if (i > 9.8 && i < 10.2);
+//   console.log(i);
+// }
+
+// let randomString = 'Доброго вечора ми з України!';
+// console.log(randomString);
+// randomString = randomString.replace('Доброго вечора', 'Доброго ранку');
+// console.log(randomString);
+
+// let testWord = 'Samsung';
+// console.log(testWord);
+// let normalizedTestWord = testWord.charAt(0).toLowerCase();
+// console.log(normalizedTestWord + testWord.slice(1, testWord.length));
+
+// let indexWord = 'Welcome to the hell';
+// console.log(indexWord.indexOf('t'));
+
+// const welcomeMessage = 'Welcom my friend!';
+// console.log(welcomeMessage.includes('my'));
+
+// const letterToCompareOne = 'a';
+// console.log(letterToCompareOne.codePointAt());
+// const letterToCompareTwo = 'A';
+// console.log(letterToCompareTwo.charCodeAt());
+// console.log(letterToCompareOne > letterToCompareTwo);
